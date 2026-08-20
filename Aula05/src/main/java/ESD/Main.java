4105eb9d63ccafb59e1a478c1d5dd179983b72e4
GabrielSpanholAbreu <gabriel.a2004@aluno.ifsc.edu.br>
2026-08-20 18:51:47 -0300

    Corrigindo exercicio da aula 05

--- Aula05/src/main/java/ESD/Main.java
+++ Aula05/src/main/java/ESD/Main.java
@@ -12,15 +12,7 @@ public class Main {
 
         for (int i = 0; i < quantidade; i++) {
             int valorSorteado = random.nextInt(100);
-
-            if (v1.valorUnico(valorSorteado)){
-                v1.inserir(valorSorteado);
-            } else {
-                while (!v1.valorUnico(valorSorteado)){
-                    valorSorteado = random.nextInt(100);
-                }
-                v1.inserir(valorSorteado);
-            }
+            v1.inserir(i);
         }
 
 
