4b926f9363687a9274a6e045fe30148fccaeaafe
Gabriel <gabrielskoob@gmail.com>
2026-08-27 17:53:28 -0300

    Inserção do código implementando o Arrays.binarySearch

--- atividade01/src/main/java/ESD/Main.java
+++ atividade01/src/main/java/ESD/Main.java
@@ -1,6 +1,8 @@
 package ESD;
 
-import java.lang.foreign.ValueLayout;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -13,21 +15,21 @@ public class Main {
 
         //Preenchendo o v1
         for (int i = 0; i < v1.getLenght(); i++) {
-            int valorSorteado = random.nextInt(1000000);
+            int valorSorteado = random.nextInt(10000000);
             v1.inserirOrdenado(valorSorteado);
         }
         //v1.imprimir();
 
         //Preenchendo o v2
         for (int i = 0; i < v2.getLenght(); i++) {
-            int valorSorteado = random.nextInt(1000000);
+            int valorSorteado = random.nextInt(10000000);
             v2.inserirOrdenado(valorSorteado);
         }
         //v2.imprimir();
 
         //Preenchendo o v3
         for (int i = 0; i < v3.getLenght(); i++) {
-            int valorSorteado = random.nextInt(1000000);
+            int valorSorteado = random.nextInt(10000000);
             v3.inserirOrdenado(valorSorteado);
         }
         //v3.imprimir();
@@ -131,6 +133,26 @@ public class Main {
             System.out.println("Comparacoes realizadas v3: " + v3.mostrarComparacoes());
             System.out.println("====================================================");
         }
+
+        // ============ Analise final ============
+
+        Vetor<Integer> v4 = new Vetor<>(100000);
+
+        for (int i = 0; i < 100000; i++) {
+            int valorSorteado = random.nextInt(10000000);
+            v4.inserirOrdenado(valorSorteado);
+        }
+
+        int[] ArrayV4 = new int[v4.obterTamanho()];
+        for (int i = 0; i < ArrayV4.length; i++) {
+            ArrayV4[i] = v4.ler(i);
+        }
+
+        long inicio = System.nanoTime();
+        int pos = Arrays.binarySearch(ArrayV4, ArrayV4[ArrayV4.length - 1]);
+        long fim = System.nanoTime();
+        long duracaoMs = (fim- inicio) / 1000000;
+        System.out.println("Tempo Arrays.binarySearch: " + duracaoMs + " ms");
 
     }
 }
